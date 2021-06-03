1e5ae8b46663ee6be1a2603a4436917723dea3e5
Marcel Fabian Krüger <tex@2krueger.de>
2021-06-03 09:16:17 +0200

    Simpler and faster Lua code

--- pdftexlua.lua
+++ pdftexlua.lua
@@ -1,34 +1,17 @@
 require('expl3.lua')
-local gsub=string.gsub
-local char =string.char
-local byte       = string.byte
-local utfchar = unicode.utf8.char
-local utfbyte = unicode.utf8.byte
-local utfgsub = unicode.utf8.gsub
-
-pdftexlua={}
-
-local function byte_to_utf(ch)
-    return utfchar(byte(ch))
-end
-pdftexlua.byte_to_utf=byte_to_utf
+local char    = string.char
+local byte    = string.byte
+local utfchar = utf8.char
+local utfbyte = utf8.codepoint
 
 local function fake_utf_read(buf)
-    return gsub(buf,"(.)", pdftexlua.byte_to_utf)
+    return utfchar(byte(buf, 1, -1))
 end
-pdftexlua.fake_utf_read = fake_utf_read
-
-
-local function utf_to_byte(ch)
-    return char(utfbyte(ch))
-end
-pdftexlua.utf_to_byte = utf_to_byte
 
 local function fake_utf_write(buf)
-    return utfgsub(buf,"(.)", pdftexlua.utf_to_byte)
+    return char(utfbyte(buf, 1, -1))
 end
-pdftexlua.fake_utf_write = fake_utf_write
 
-luatexbase.add_to_callback('process_output_buffer', pdftexlua.fake_utf_write, "utf8 writing")
-luatexbase.add_to_callback('process_input_buffer', pdftexlua.fake_utf_read, "utf8 reading")
+luatexbase.add_to_callback('process_output_buffer', fake_utf_write, "utf8 writing")
+luatexbase.add_to_callback('process_input_buffer', fake_utf_read, "utf8 reading")
 
