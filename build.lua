20f7af1fddf53658876a2526b4b1792b3f42089a
David Carlisle <d.p.carlisle@gmail.com>
2023-01-22 14:31:13 +0000

    l3build

---
+++ build.lua
@@ -0,0 +1,10 @@
+module = "xpdftex"
+
+sourcefiles={"xpdftex.tex","xpdftex.lua","xpdflatex.ini"}
+installfiles={"xpdftex.tex","xpdftex.lua","xpdflatex.ini"}
+
+tdslocations={
+"tex/xpdftex/base/xpdftex.tex",
+"tex/xpdftex/base/xpdftex.lua",
+"tex/xpdftex/base/xpdftex.ini"
+}
